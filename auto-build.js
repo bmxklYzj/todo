8c7439f9726b5015ada73321bf01cdf33d617543
yangzongjun <yangzongjun@baidu.com>
2018-11-25 15:11:35 +0800

    remove nouse file

--- auto-build.js
+++ auto-build.js
@@ -1,16 +1,16 @@
 /**
  * @file 自动部署项目到vps：
- * 1. 在vps上clone整个项目： git clone git@github.com:bmxklYzj/demo-exercise.git
- * 2. 打包： node demo-exercise/2018-11-todo/auto-build.js
+ * 1. 在vps上clone整个项目： git clone https://github.com/bmxklYzj/todo.git
+ * 2. 打包： cd todo; node auto-build.js
  */
 
 var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
 
-exec(`rm -rf ../../../todo; cp -rf ../todo ../../../;cd ../../../todo; pwd`, function(err, stdout, stderr) {
-    console.log(err, stdout, stderr);
-});
+// exec(`rm -rf ../../../todo; cp -rf ../todo ../../../;cd ../../../todo; pwd`, function(err, stdout, stderr) {
+//     console.log(err, stdout, stderr);
+// });
 
 fs.readdirSync(__dirname, 'utf8')
     .filter(item => fs.statSync(path.join(__dirname, item)).isDirectory())
